591f8528d48859a1a2c9b7631eb39d43c9791981
StreetStrider <teamfortresslife@gmail.com>
2015-05-18 00:34:43 +0300

    make rootpath.path enumerable, when 'resolve' & 'partial' not-enumerable: for clear inspection.

--- src/Rootpath.js
+++ src/Rootpath.js
@@ -10,16 +10,19 @@ module.exports = function Rootpath (/* [path, or path[], ...] */)
 		return flatres(root, arguments);
 	}
 
-	Object.defineProperty(rootpath, 'path', { value: root });
+	enumvalue(rootpath, 'path', root);
 
-	rootpath.resolve = function resolve (/* [path, or path[], ...] */)
+	var resolve = function resolve (/* [path, or path[], ...] */)
 	{
 		return rootpath(arguments);
 	}
-	rootpath.partial = function partial (/* [path, or path[], ...] */)
+	value(rootpath, 'resolve', resolve);
+
+	var partial = function partial (/* [path, or path[], ...] */)
 	{
 		return Rootpath(rootpath(arguments));
 	}
+	value(rootpath, 'partial', partial);
 
 	return rootpath;
 }
@@ -32,4 +35,15 @@ var
 function flatres ()
 {
 	return path.resolve.apply(null, flat(arguments));
+}
+
+
+function value (object, key, value)
+{
+	Object.defineProperty(object, key, { value: value });
+}
+
+function enumvalue (object, key, value)
+{
+	Object.defineProperty(object, key, { value: value, enumerable: true });
 }
