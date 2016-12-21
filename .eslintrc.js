10da896b42adc92fe2fc8f0944ec0bce8e52c969
Strider <strange.transistor@gmail.com>
2016-12-22 02:42:39 +0300

    js-outlander = 2.0.9.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 
 module.exports =
 {
-	extends: './node_modules/js-outlander/outlander.eslint.js'
+	extends: './node_modules/js-outlander/outlander.eslint.js',
+
+	rules:
+	{
+		'prefer-arrow-callback': 0 /* node v0.12 :) */
+	}
 }
