3583b999f795eaac08dcbeab39af870d484f9e50
Strider <strange.transistor@gmail.com>
2017-10-06 00:24:52 +0300

    tidy.

--- rootpath.ts
+++ rootpath.ts
@@ -1,5 +1,5 @@
-/// <reference path="index.d.ts" />
-/// <reference path="./node_modules/def-prop/def.d.ts" />
+/// <reference path='index.d.ts' />
+/// <reference path='node_modules/def-prop/def.d.ts' />
 
 ; export type Rootpath$Segment = string | $Rootpath
 
