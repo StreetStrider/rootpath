d74e4c8bcfd060c8e648652c818f9e8e1781813b
Strider <strange.transistor@gmail.com>
2020-01-05 16:24:41 +0300

    rm some ambient types, defer to allowSyntheticDefaultImports.

--- ambient.d.ts
+++ ambient.d.ts
@@ -1,25 +1,4 @@
 
-declare module 'find-root'
-{
-	type FindRootCheckFn = (dir: string) => boolean
-
-	export default function findRoot(startingPath: string, check?: FindRootCheckFn): string
-}
-
-declare module 'globjoin'
-{
-	export default function globjoin(...globs: string[]): string
-	;
-	export default function globjoin(...globs: Array<string | string[]>): string[]
-}
-
-declare module 'lodash.flattendeep'
-{
-	import { flattenDeep } from 'lodash'
-
-	export default flattenDeep
-}
-
 declare module 'node-path-extras'
 {
 	interface Extras
