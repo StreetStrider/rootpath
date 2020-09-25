c2beec44f7884308da0790a301aa56ce3c86d5e6
Strider <strange.transistor@gmail.com>
2020-09-26 00:34:29 +0300

    update eslint and outlander, use eslint typescript.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,19 @@ module.exports =
 	extends: require.resolve('js-outlander'),
 
 	parser: '@typescript-eslint/parser',
-
 	parserOptions:
 	{
 		sourceType: 'module',
-	}
+	},
+
+	plugins:
+	[
+		'@typescript-eslint',
+	],
+
+	rules:
+	{
+		'@typescript-eslint/no-unused-vars': 2,
+		// '@typescript-eslint/no-unused-expressions': [ 2, { allowShortCircuit: true } ],
+	},
 }
--- test/types.ts
+++ test/types.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 import Rootpath from '../rootpath'
 
