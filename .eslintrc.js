c7f0c060ef7370e929dbf2f4c59bec08e8892e4f
Strider <strange.transistor@gmail.com>
2020-10-03 01:31:58 +0300

    use js-outlander = 2.10 with typescript.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,5 @@
 
 module.exports =
 {
-	extends: require.resolve('js-outlander'),
-
-	parser: '@typescript-eslint/parser',
-	parserOptions:
-	{
-		sourceType: 'module',
-	},
-
-	plugins:
-	[
-		'@typescript-eslint',
-	],
-
-	rules:
-	{
-		'no-unused-vars': 0,
-		'@typescript-eslint/no-unused-vars': 2,
-
-		'no-unused-expressions': 0,
-		'@typescript-eslint/no-unused-expressions': 2,
-	},
+	extends: require.resolve('js-outlander/typescript'),
 }
